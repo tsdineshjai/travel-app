f4562b2a6850434f6d43a063accee09deba60160
tanishka <tsdineshjai@gmail.com>
2022-04-15 14:01:22 +0530

    Updates

---
+++ precache-manifest.63850b2b1c7f1ea6b9659b63f8a4f1ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab569c5ce8762caab952",
+    "url": "/travel-app/static/css/main.63ebbf1e.chunk.css"
+  },
+  {
+    "revision": "ab569c5ce8762caab952",
+    "url": "/travel-app/static/js/main.ab569c5c.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/travel-app/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "c205f317184907ce0b41",
+    "url": "/travel-app/static/js/2.c205f317.chunk.js"
+  },
+  {
+    "revision": "dc1f10b58cc99d926bde",
+    "url": "/travel-app/static/js/runtime~main.dc1f10b5.js"
+  },
+  {
+    "revision": "43059661c7828d026ab97dca41370e31",
+    "url": "/travel-app/index.html"
+  }
+];
